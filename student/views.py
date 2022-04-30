98be4d6ad46a3edbedde7421bc857529e4855e63
M-Rafay <mrafay.dev@gmail.com>
2022-04-30 23:48:47 +0500

    changes in student response data

--- student/views.py
+++ student/views.py
@@ -4,8 +4,20 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from student.models import Student
 from student.serializers import StudentSerializer
+from rest_framework.response import Response
 
 
 class studentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"students" : serializer.data})
